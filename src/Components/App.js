957e2761438d3b0c172f960da816df6ea935a503
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2018-11-30 10:56:46 +0900

    Super simple but it does the job

---
+++ src/Components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import GlobalStyle from "./GlobalStyles";
+
+class App extends Component {
+  render() {
+    return (
+      <>
+        <GlobalStyle />
+      </>
+    );
+  }
+}
+
+export default App;
