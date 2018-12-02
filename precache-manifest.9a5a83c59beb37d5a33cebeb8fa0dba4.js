57ed0f8f40f928ea6213b8e03fdfc15e4e88fb21
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2018-12-02 12:09:18 +0900

    Updates

---
+++ precache-manifest.9a5a83c59beb37d5a33cebeb8fa0dba4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee43e926fa32d20f7851",
+    "url": "/karakoy/static/js/main.ee43e926.chunk.js"
+  },
+  {
+    "revision": "1e968c0f846bc06e99b9",
+    "url": "/karakoy/static/css/1.139f9941.chunk.css"
+  },
+  {
+    "revision": "1e968c0f846bc06e99b9",
+    "url": "/karakoy/static/js/1.1e968c0f.chunk.js"
+  },
+  {
+    "revision": "7cf80b3dc4b3ca84686e",
+    "url": "/karakoy/static/js/runtime~main.7cf80b3d.js"
+  },
+  {
+    "revision": "7f2c601b18ae11d9eaa0ee7429374f6c",
+    "url": "/karakoy/index.html"
+  }
+];
